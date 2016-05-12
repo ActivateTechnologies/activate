b8ed8177659548b8ab8ed8ad1f9669c7995b3299
ajones5876 <ajones.5876@gmail.com>
2016-05-13 00:29:07 +0100

    Moves integration, half working. Having to solve deep linking back to Ionic App.

--- app/pages/profilepage/profilepage.ts
+++ app/pages/profilepage/profilepage.ts
@@ -435,7 +435,7 @@ export class ProfilePage {
             }
         });
         browserRef.addEventListener("exit", function(event) {
-            reject("The Strava sign in flow was canceled");
+            reject("The Strava sign in flow was cancelled");
         });
     });
   }
@@ -463,6 +463,63 @@ export class ProfilePage {
     xmlhttp.send(params);
     alert(4);
   }
+
+  //MOVES
+  connectMoves() {
+    alert(1);
+    return new Promise(function(resolve, reject) {
+        var browserRef = window.cordova.InAppBrowser.open("moves://app/authorize?client_id=95C57N4Gt5t9l5uir45i0P6RcNd1DN6v&redirect_uri=com.ionicframework.activate804698&scope=activity", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+        browserRef.addEventListener("loadstart", (event) => {
+            //alert(1);
+            if ((event.url).indexOf("http://localhost") === 0) {
+                browserRef.removeEventListener("exit", (event) => {});
+                alert(2);
+                alert(event.url);
+                browserRef.close();
+                var url = event.url
+                var accessCode = url.substring(30,url.length);
+                alert(accessCode);
+                console.log(accessCode);
+                alert(3);
+            }
+        });
+        browserRef.addEventListener("exit", function(event) {
+            reject("The Moves sign in flow was cancelled");
+        });
+    });
+  }
+
+  MovesAPIPOST() {
+    alert('stravaAPIPOST');
+    var c_id = "11012";
+    var c_secret = "1d5dc79c5adbaaefcc6eeb2b2c9ddb584085ecfc";
+    var access_code = "c420583602c1eb00dd60707dd48c58d46e5c8a83";
+    var params = "client_id=" + c_id + "&client_secret=" + c_secret + "&code=" + access_code;
+​    
+    var xmlhttp = new XMLHttpRequest();
+    alert(1);
+    xmlhttp.onreadystatechange = function () {
+      alert('2');
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        alert(xmlhttp.responseText);
+      }
+    }
+​    alert(3);
+    xmlhttp.open("POST", "https://www.strava.com/oauth/token", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.setRequestHeader("Content-length", "3");
+    xmlhttp.setRequestHeader("Connection", "close");
+    xmlhttp.send(params);
+    alert(4);
+  }
+
+
+
+
+
+
+
+
 
   //MEETUP
   connectMeetup() {
